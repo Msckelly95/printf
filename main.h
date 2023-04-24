9c90e2bc2a2c83e2c113c7eeb8018b38fee83ab9
Msckelly95 <Msckelly95@github.com>
2023-04-23 23:36:49 -0700

    printf

--- main.h
+++ main.h
@@ -196,7 +196,6 @@ char buffer[],
 
 
 /****************** UTILS ******************/
-
 int is_printable(char);
 
 int append_hexa_code(char, char[], int);
@@ -204,9 +203,11 @@ int append_hexa_code(char, char[], int);
 int is_digit(char);
 
 
+
 long int convert_size_number(long int num, int size);
 
 long int convert_size_unsgnd(unsigned long int num, int size);
 
 
-#endif /* MAIN_H *//
+
+#endif /* MAIN_H */
